be3bcffad6b50e7f92f6c4c824c7f8f1feb695a4
chenmingxue <chenmingxue@kpmg.com>
2025-09-26 18:08:57 -0700

    search agent

---
+++ main.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt,
+)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+chain = agent_executor 
+
+def main():
+    print("Hello, LangChain!")
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
+        }
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
